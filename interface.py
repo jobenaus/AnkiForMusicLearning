9ede718429ca9b211d7da68aa0086cc33024848f
jobenaus <j.obenaus@icloud.com>
2022-06-11 17:26:48 +0200

    updated .gitignore

--- interface.py
+++ interface.py
@@ -5,21 +5,19 @@ import globals as glob
 
 # deck information
 glob.instrument = "Violine"
-glob.title = "Die Tapferkeit"
-glob.composer = "Telemann"
-glob.deck_suffix = ""
+glob.title = "Allegro moderato"
+glob.composer = "Gebauer"
+glob.deck_suffix = "(Seperate Hands)"
 
 # settings
-glob.test_mode = True
+glob.test_mode = False
 glob.override_deck = True
 
 # init
 anki_connect.init_deck()
 
 # interface
-bar_names = make_bar_names(53)
+bar_names = make_bar_names(127)
 
 add_single_bars(bar_names, separate_hands=True)
-add_multiple_bars(1,3, bar_names)
-
-
+add_multiple_bars(1, 3, bar_names)
