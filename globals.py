50a180f0dc7ad0aef7e8a7811c403e68e82cac79
jobenaus <j.obenaus@icloud.com>
2022-06-11 02:21:37 +0200

    Fileformatting and updated .gitignore

--- globals.py
+++ globals.py
@@ -1,9 +1,9 @@
-#deck informations
+# deck informations
 instrument = "instrument"
 title = "title"
 composer = "composer"
 deck_suffix = "suffix"
 
-#settings
+# settings
 test_mode = True
 override_deck = True
