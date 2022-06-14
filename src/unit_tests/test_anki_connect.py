f1fb86db07adca9a309c5a7edd1a51542892d740
jobenaus <j.obenaus@icloud.com>
2022-06-15 00:08:21 +0200

    implemented test_request_multiple_parameters

--- src/unit_tests/test_anki_connect.py
+++ src/unit_tests/test_anki_connect.py
@@ -4,9 +4,27 @@ from app.anki_connect import request
 def test_request_one_parameter():
     expected = {
         "action": "getEaseFactors",
-        "params": {"cards": [1483959291685, 1483959293217]},
         "version": 6,
+        "params": {"cards": [1483959291685, 1483959293217]},
     }
     actual = request("getEaseFactors", cards=[1483959291685, 1483959293217])
     assert actual == expected
-    
+
+
+def test_request_multiple_parameters():
+    expected = {
+        "action": "setSpecificValueOfCard",
+        "version": 6,
+        "params": {
+            "card": 1483959291685,
+            "keys": ["flags", "odue"],
+            "newValues": ["1", "-100"],
+        },
+    }
+    actual = request(
+        "setSpecificValueOfCard",
+        card=1483959291685,
+        keys=["flags", "odue"],
+        newValues=["1", "-100"],
+    )
+    assert actual == expected
