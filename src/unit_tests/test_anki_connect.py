8e1302cd4cb034d3dc8b623dff926a0d1a3a38ae
jobenaus <j.obenaus@icloud.com>
2022-06-17 10:48:50 +0200

    Added TestRequest class

--- src/unit_tests/test_anki_connect.py
+++ src/unit_tests/test_anki_connect.py
@@ -4,32 +4,36 @@ Test that the request function returns the correct request.
 from anki_connect import request
 
 
-def test_request_one_parameter():
-    """Tast that one parameter is correctly handled by request."""
-    expected = {
-        "action": "getEaseFactors",
-        "version": 6,
-        "params": {"cards": [1483959291685, 1483959293217]},
-    }
-    actual = request("getEaseFactors", cards=[1483959291685, 1483959293217])
-    assert actual == expected
+class TestRequest:
+    """Tests the request function with one or more parameters."""
+
+    def test_request_one_parameter(self):
+        """Tast that one parameter is correctly handled by request."""
+        expected = {
+            "action": "getEaseFactors",
+            "version": 6,
+            "params": {"cards": [1483959291685, 1483959293217]},
+        }
+        actual = request("getEaseFactors", cards=[1483959291685, 1483959293217])
+        assert actual == expected
+
+    def test_request_multiple_parameters(self):
+        """Test that multiple parameters are correctly handled by request."""
+        expected = {
+            "action": "setSpecificValueOfCard",
+            "version": 6,
+            "params": {
+                "card": 1483959291685,
+                "keys": ["flags", "odue"],
+                "newValues": ["1", "-100"],
+            },
+        }
+        actual = request(
+            "setSpecificValueOfCard",
+            card=1483959291685,
+            keys=["flags", "odue"],
+            newValues=["1", "-100"],
+        )
+        assert actual == expected
 
 
-def test_request_multiple_parameters():
-    """Test that multiple parameters are correctly handled by request."""
-    expected = {
-        "action": "setSpecificValueOfCard",
-        "version": 6,
-        "params": {
-            "card": 1483959291685,
-            "keys": ["flags", "odue"],
-            "newValues": ["1", "-100"],
-        },
-    }
-    actual = request(
-        "setSpecificValueOfCard",
-        card=1483959291685,
-        keys=["flags", "odue"],
-        newValues=["1", "-100"],
-    )
-    assert actual == expected
